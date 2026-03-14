3749a288f3b3626b5e4759b470339190b1a9fd8d
Daichi Tezuka <pastamac@tedzukataijinoMacBook-Air.local>
2026-03-14 16:22:45 +0900

    フォント改善。jsファイルを別途作成しそっちをいじることで、workの更新が可能に。

---
+++ worksData.js
@@ -0,0 +1,15 @@
+const worksList = [
+    {
+        title: "作品 01: デジタルアート",
+        desc: "2024年に制作。マウス操作で色が変わる仕掛けがあります。",
+        img: "WORKS/work_1.jpg",
+        sound: "boing"
+    },
+    {
+        title: "作品 02: アナログ回路",
+        desc: "手描きの回路図をもとに、実際に音が出るデバイスを試作。",
+        img: "WORKS/work_2.jpg",
+        sound: "pop"
+    },
+    // 新しい作品を追加するときは、ここをコピーして増やすだけ！
+];
